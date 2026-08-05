50b0e8edc85684424b9465c9f268c89ba5c3ae6c
Lutiepromax <158442488+Lutiepromax@users.noreply.github.com>
2026-08-05 23:53:15 +0300

    Add absorber state block

---
+++ v122src/com/radel/obsidiantnt/block/DamagedAbsorberBlock.java
@@ -0,0 +1,10 @@
+package com.radel.obsidiantnt.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+
+public final class DamagedAbsorberBlock extends Block {
+    public DamagedAbsorberBlock(AbstractBlock.Properties properties) {
+        super(properties);
+    }
+}
